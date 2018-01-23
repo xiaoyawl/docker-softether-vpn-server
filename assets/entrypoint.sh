faf36042f129669979f3e86978fbaa92a7801907
LookBack <mondeolove@gmail.com>
2018-01-23 16:42:43 +0800

    change entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,24 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+#########################################################################
+# File Name: entrypoint.sh
+# Author: LookBack
+# Email: admin#dwhd.org
+# Version:
+# Created Time: 2018年01月23日 星期二 16时40分56秒
+#########################################################################
+
 set -e
 
 if [ "x$1" = 'x/usr/vpnserver/vpnserver' ]; then
 
-    # Linking Logs
-    for d in server_log security_log packet_log;
-    do
-        if [ ! -L /usr/vpnserver/$d ]; then
-          mkdir -p /var/log/vpnserver/$d
-          ln -s /var/log/vpnserver/$d /usr/vpnserver/$d
-        fi
-    done
+	# Linking Logs
+	for d in server_log security_log packet_log; do
+		if [ ! -L /usr/vpnserver/$d ]; then
+			mkdir -p /var/log/vpnserver/$d
+			ln -s /var/log/vpnserver/$d /usr/vpnserver/$d
+		fi
+	done
 
     # Allow app to use ports < 1024 without root
-    chown -R softether:softether /usr/vpnserver
-    setcap 'cap_net_bind_service=+ep' /usr/vpnserver/vpnserver
+	chown -R softether:softether /usr/vpnserver
+	setcap 'cap_net_bind_service=+ep' /usr/vpnserver/vpnserver
 
     # Starting
-    echo "Starting SoftEther VPN Server"
-    exec su-exec softether sh -c "`echo $@`"
+	echo "Starting SoftEther VPN Server"
+	exec su-exec softether sh -c "`echo $@`"
 else
-    exec "$@"
+	exec "$@"
 fi
